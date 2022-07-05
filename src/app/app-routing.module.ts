e4fe9f515d68b4274375f0e3a2cc103f8fdc0531
Marek Kotulski <marek.kotulski@febrit.co>
2022-07-05 16:01:32 +0200

    add structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'content', pathMatch: 'full' },
+  { path: 'content', component: ContentComponent },
+  { path: '**', redirectTo: 'content', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { SearchComponent } from './content/search/search.component';
+import { WallComponent } from './content/wall/wall.component';
+import { PostComponent } from './content/wall/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    SearchComponent,
+    WallComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
